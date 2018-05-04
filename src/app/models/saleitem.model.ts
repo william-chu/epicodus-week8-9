976a88bb55198873a35a8b2039e977317488049c
William Chu <Williamchu51@gmail.com>
2018-05-04 13:05:19 -0700

    add text for out of stock items

--- src/app/models/saleitem.model.ts
+++ src/app/models/saleitem.model.ts
@@ -1,6 +1,13 @@
 export class SaleItem {
-  constructor(public price: number, public name: string, public imgMain: string, public imgAlt: string, public productType: string, public inventory: number) {
+  inStock = this.checkInstock();
+  constructor(public price: number, public name: string, public imgMain: string, public imgAlt: string, public productType: string, public inventory: number) {}
 
+  checkInstock() {
+    if (this.inventory > 0) {
+      return true;
+    } else {
+      return null;
+    }
   }
 
 }
