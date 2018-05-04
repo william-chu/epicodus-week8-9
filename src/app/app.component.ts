5988d87ce5288f2f4bb68da02c77e1ac3c2873aa
William Chu <Williamchu51@gmail.com>
2018-05-04 09:03:13 -0700

    add saleItems array of static SaleItem objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,12 @@ import { SaleItem } from './models/saleitem.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Neuw Denim Angular Rebuild';
+  saleItems: SaleItem[] = [
+    new SaleItem(159, "LOU SLIM - RAW SELVEDGE", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/lou_slim_pant_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/lou_slim_pant_alt.jpg", "pant"),
+    new SaleItem(149,"IGGY SKINNY - PERFECTO", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/iggy_skinny_pant_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/iggy_skinny_pant_alt.jpg", "pant"),
+    new SaleItem(89,"UNITY TEE - NAVY LINEN STRIPE", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/unity_tee_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/unity_tee_alt.jpg", "tee"),
+    new SaleItem(199,"TYPE ONE JACKET - BEATEN INDIGO", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/type_one_jacket_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/type_one_jacket_alt.jpg", "pant"),
+    new SaleItem(649,"RIDER JACKET - BLACK LEATHER", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/rider_jacket_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/rider_jacket_alt.jpg", "pant"),
+  ]
+
 }
--- src/app/models/saleitem.model.ts
+++ src/app/models/saleitem.model.ts
@@ -2,8 +2,8 @@ export class SaleItem {
   inStock: boolean = true;
   inventory: number = 1;
 
-  constructor(public price: number, public imgMain: string, public imgAlt: string, productType: string) {
+  constructor(public price: number, public name: string, public imgMain: string, public imgAlt: string, productType: string) {
 
   }
-  
+
 }
