654ae7496080f00ad1a054fb1a3832be61cd6d60
William Chu <Williamchu51@gmail.com>
2018-05-05 08:50:18 -0700

    configure home page route, edit navbar links, copy dislay-sale-items component into mens-sale

--- src/app/mens-sale/mens-sale.component.ts
+++ src/app/mens-sale/mens-sale.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SaleItem } from './../models/saleitem.model';
 
 @Component({
   selector: 'app-mens-sale',
   templateUrl: './mens-sale.component.html',
   styleUrls: ['./mens-sale.component.css']
 })
-export class MensSaleComponent implements OnInit {
+export class MensSaleComponent {
+  @Input() childSaleItems: SaleItem[];
 
-  constructor() { }
-
-  ngOnInit() {
+  filterSaleItems: string ="all";
+  onChange(filterBy) {
+    this.filterSaleItems = filterBy;
   }
 
+  outOfStock(saleItem) {
+    if (saleItem.inventory === 0) {
+      return 'out-of-stock';
+    }
+  }
+
+  mouseOut(saleItem) {
+    saleItem.displayImage = saleItem.imgMain;
+  }
+
+  mouseOver(saleItem) {
+    saleItem.displayImage = saleItem.imgAlt;
+  }
 }
