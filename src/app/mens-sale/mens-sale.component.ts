866c3b98d11ab5855669d120e7f0f4acccaf34e1
William Chu <Williamchu51@gmail.com>
2018-05-05 09:05:15 -0700

    configure mens-sale route, undo display-sale-items component copy

--- src/app/mens-sale/mens-sale.component.ts
+++ src/app/mens-sale/mens-sale.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { SaleItem } from './../models/saleitem.model';
 
 @Component({
@@ -7,24 +7,5 @@ import { SaleItem } from './../models/saleitem.model';
   styleUrls: ['./mens-sale.component.css']
 })
 export class MensSaleComponent {
-  @Input() childSaleItems: SaleItem[];
-
-  filterSaleItems: string ="all";
-  onChange(filterBy) {
-    this.filterSaleItems = filterBy;
-  }
-
-  outOfStock(saleItem) {
-    if (saleItem.inventory === 0) {
-      return 'out-of-stock';
-    }
-  }
-
-  mouseOut(saleItem) {
-    saleItem.displayImage = saleItem.imgMain;
-  }
-
-  mouseOver(saleItem) {
-    saleItem.displayImage = saleItem.imgAlt;
-  }
+  
 }
