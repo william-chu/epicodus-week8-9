9cc6a741701f6952c321ac5d7cfe4432e38a6fd1
William Chu <Williamchu51@gmail.com>
2018-05-05 10:32:12 -0700

    add productId prop to SaleItem model, add route to mens-sale component

--- src/app/mens-sale/mens-sale.component.ts
+++ src/app/mens-sale/mens-sale.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { SaleItem } from './../models/saleitem.model';
 
@@ -9,12 +10,12 @@ import { SaleItem } from './../models/saleitem.model';
 })
 export class MensSaleComponent {
   saleItems: SaleItem[] = [
-    new SaleItem(159, "LOU SLIM - RAW SELVEDGE", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/lou_slim_pant_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/lou_slim_pant_alt.jpg", "pant", 1),
-    new SaleItem(149,"IGGY SKINNY - PERFECTO", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/iggy_skinny_pant_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/iggy_skinny_pant_alt.jpg", "pant", 0),
-    new SaleItem(69,"NOISE TEE - TEENAGE RIOT", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/noise_tee_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/noise_tee_alt.jpg", "tee", 1),
-    new SaleItem(89,"UNITY TEE - NAVY LINEN STRIPE", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/unity_tee_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/unity_tee_alt.jpg", "tee", 0),
-    new SaleItem(199,"TYPE ONE JACKET - BEATEN INDIGO", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/type_one_jacket_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/type_one_jacket_alt.jpg", "jacket", 1),
-    new SaleItem(649,"RIDER JACKET - BLACK LEATHER", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/rider_jacket_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/rider_jacket_alt.jpg", "jacket", 1),
+    new SaleItem(30796, 159, "LOU SLIM - RAW SELVEDGE", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/lou_slim_pant_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/lou_slim_pant_alt.jpg", "pant", 1),
+    new SaleItem(32284, 149,"IGGY SKINNY - PERFECTO", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/iggy_skinny_pant_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/iggy_skinny_pant_alt.jpg", "pant", 0),
+    new SaleItem(32851, 69,"NOISE TEE - TEENAGE RIOT", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/noise_tee_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/noise_tee_alt.jpg", "tee", 1),
+    new SaleItem(32817, 89,"UNITY TEE - NAVY LINEN STRIPE", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/unity_tee_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/unity_tee_alt.jpg", "tee", 0),
+    new SaleItem(32811, 199,"TYPE ONE JACKET - BEATEN INDIGO", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/type_one_jacket_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/type_one_jacket_alt.jpg", "jacket", 1),
+    new SaleItem(32769, 649,"RIDER JACKET - BLACK LEATHER", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/rider_jacket_main.jpg", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/rider_jacket_alt.jpg", "jacket", 1),
   ];
 
   filterSaleItems: string ="all";
@@ -34,5 +35,9 @@ export class MensSaleComponent {
 
   mouseOver(saleItem) {
     saleItem.displayImage = saleItem.imgAlt;
+  }
+
+  goToSaleItemDetail(clickedSaleItem: SaleItem) {
+    this.router.navigate(['saleItems'], clickedSaleItem.productId]);
   }
 }
