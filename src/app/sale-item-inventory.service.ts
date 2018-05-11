e6c387d0dce6651b0421572cba27c7cba274885b
William Chu <williamchu51@gmail.com>
2018-05-11 08:56:43 -0700

    remove sale-item-inventory.ts and import statement in sale-item-inventory service

--- src/app/sale-item-inventory.service.ts
+++ src/app/sale-item-inventory.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { SaleItem } from './models/saleitem.model';
-import { SALEITEMSMENS, SALEITEMSWOMENS } from './sale-item-inventory';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
