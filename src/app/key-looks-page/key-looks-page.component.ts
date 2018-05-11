e05cfce7a54653aaf95fd9f60e9ff217d0247ac6
William Chu <williamchu51@gmail.com>
2018-05-11 10:27:51 -0700

    implement key-look service with Firebase data in key-looks-page component

--- src/app/key-looks-page/key-looks-page.component.ts
+++ src/app/key-looks-page/key-looks-page.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { KeyLookService } from './../key-look.service';
 import { KeyLook } from './../models/keylook.model';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-key-looks-page',
   templateUrl: './key-looks-page.component.html',
-  styleUrls: ['./key-looks-page.component.css']
+  styleUrls: ['./key-looks-page.component.css'],
+  providers: [KeyLookService]
 })
 export class KeyLooksPageComponent implements OnInit {
-  keyLooks: KeyLook[] = [
-    new KeyLook("08 MAY 2018", "Denim & details - Style them up, pair them back, or simply wear them off the the hanger. Just make them your own.", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/key_looks_01.jpg"),
-    new KeyLook("07 MAY 2018", "FORM Denim - Made from music. Made for movement.", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/key_looks_02.jpg"),
-    new KeyLook("29 APRIL 2018", "Keep it simple.", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/key_looks_03.jpg"),
-    new KeyLook("24 APRIL 2018", "Inspired by the past. Recreated for the present. Cherished for the future. #NeuwDenim", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/key_looks_04.jpg"),
-    new KeyLook("22 APRIL 2018", "Threaded to the beat. @deathbells_ drummer Sam in our Lux Jacket. Tap to buy direct.", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/key_looks_05.jpg"),
-    new KeyLook("22 APRIL 2018", "Turn on. Tune in. Drop out...", "https://raw.githubusercontent.com/william-chu/epicodus-week8-9/master/src/assets/key_looks_06.jpg"),
-  ];
+  constructor(private keyLookService: KeyLookService) { }
 
-  constructor() { }
+  keyLooks: FirebaseListObservable<any[]>;
 
   ngOnInit() {
+    this.keyLooks = this.keyLookService.getKeyLooks();
   }
 
 }
