97ffedca22b3e01d28f6eeb4147b64e3bcc040a9
William Chu <williamchu51@gmail.com>
2018-05-11 14:10:41 -0700

    add column toggle buttons

--- src/app/mens-sale/mens-sale.component.ts
+++ src/app/mens-sale/mens-sale.component.ts
@@ -14,14 +14,24 @@ export class MensSaleComponent implements OnInit {
   constructor(private router: Router, private saleItemInventoryService: SaleItemInventoryService) {}
 
   saleItems: FirebaseListObservable<any[]>;
+  filterParameter: string ="all";
+  displayColumns: string = "item-display col-4";
 
   ngOnInit() {
     this.saleItems = this.saleItemInventoryService.getSaleItemInventoryMens();
   }
 
-  filterParameter: string ="all";
   setFilterParameter(filterBy) {
     this.filterParameter = filterBy;
+  }
+
+  setDisplayColumns(columns) {
+    if (columns === 3) {
+      this.displayColumns = "item-display col-4";
+    }
+    else if (columns === 4) {
+      this.displayColumns = "item-display col-3"
+    }
   }
 
   outOfStock(saleItem) {
--- src/app/womens-sale/womens-sale.component.ts
+++ src/app/womens-sale/womens-sale.component.ts
@@ -14,14 +14,24 @@ export class WomensSaleComponent implements OnInit {
   constructor(private router: Router, private saleItemInventoryService: SaleItemInventoryService) {}
 
   saleItems: FirebaseListObservable<any[]>;
+  filterParameter: string = "all";
+  displayColumns = "item-display col-4"
 
   ngOnInit() {
     this.saleItems = this.saleItemInventoryService.getSaleItemInventoryWomens();
   }
 
-  filterParameter: string ="all";
   setFilterParameter(filterBy) {
     this.filterParameter = filterBy;
+  }
+
+  setDisplayColumns(columns) {
+    if (columns === 3) {
+      this.displayColumns = "item-display col-4";
+    }
+    else if (columns === 4) {
+      this.displayColumns = "item-display col-3"
+    }
   }
 
   outOfStock(saleItem) {
